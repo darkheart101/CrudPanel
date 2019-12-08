0d4ffb2f0a80c3039e64a00537588ef113c631dd
darkheart101 <kurokokoro@windowslive.com>
2019-12-08 09:39:27 +0200

    feat - Create Model by UI

--- src/Http/Controllers/CrudPanelController.php
+++ src/Http/Controllers/CrudPanelController.php
@@ -4,6 +4,7 @@ namespace tkouleris\CrudPanel\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Artisan;
 
 class CrudPanelController extends Controller
 {
@@ -14,6 +15,24 @@ class CrudPanelController extends Controller
 
     public function create_model(Request $request)
     {
-        return $request->all();
+        if(($request->model_name == null) || (!$request->has('model_name')) )
+        {
+            $results['success'] = false;
+            $results['message'] = "No model name selected!";
+            return $results;
+        }
+
+        if ($request->model_name == trim($request->model_name) && strpos($request->model_name, ' ') !== false) {
+            $results['success'] = false;
+            $results['message'] = "Model name must not contain spaces!";
+            return $results;
+        }
+
+        Artisan::call('make:model '.$request->model_name);
+        $message = Artisan::output();
+
+        $results['success'] = true;
+        $results['message'] = $message;
+        return $results;
     }
 }
--- src/views/crud_panel_index.blade.php
+++ src/views/crud_panel_index.blade.php
@@ -1,15 +1,49 @@
 <html>
     <head>
+        <script
+            src="https://code.jquery.com/jquery-3.4.1.min.js"
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+            crossorigin="anonymous">
+        </script>
         <title>Crud Panel</title>
     </head>
 
     <body>
         <h1>Welcome to Crud Panel</h1>
-        <form action="model/create" method="post">
+        <form action="" method="post">
                 @csrf
                 <input type="text" name="model_name">
-                <input type="submit" value="Submit">
+                <input type="submit" name="btn_submit_model" value="Submit">
         </form>
     </body>
+
+    <script>
+        $(document).ready(function() {
+
+            $("input[name=btn_submit_model]").click(function(event){
+                event.preventDefault();
+
+                var model_name = $('input[name=model_name]').val();
+                var _token = $('input[name=_token]').val();
+
+                jQuery.ajax({
+                    url: "model/create",
+                    method: 'post',
+                    data: { _token: _token, model_name: model_name },
+                    success: function(response)
+                    {
+                        alert(response.message);
+                    },
+                    error: function (response)
+                    {
+                        alert(response.message);
+                    }
+                });
+
+            });
+
+        });
+
+    </script>
 
 </html>
