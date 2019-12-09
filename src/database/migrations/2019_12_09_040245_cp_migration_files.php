8f08efeee153631e093d108d95cfbbe3750db064
darkheart101 <kurokokoro@windowslive.com>
2019-12-09 06:06:47 +0200

    chore - Migrations

--- src/database/migrations/2019_12_09_040245_cp_migration_files.php
+++ src/database/migrations/2019_12_09_040245_cp_migration_files.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateModelFilesTable extends Migration
+class CpMigrationFiles extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateModelFilesTable extends Migration
      */
     public function up()
     {
-        Schema::create('cp_model_files', function (Blueprint $table) {
-            $table->bigIncrements('ModelFileId');
-            $table->string('ModelFileName');
+        Schema::create('cp_migration_files', function (Blueprint $table) {
+            $table->bigIncrements('MigrationFileId');
+            $table->string('MigrationFileName');
+            $table->bigInteger('MigrationModelId');
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ class CreateModelFilesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('cp_model_files');
+        Schema::dropIfExists('cp_migration_files');
     }
 }
